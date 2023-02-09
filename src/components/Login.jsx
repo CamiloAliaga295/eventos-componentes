93fdbc99dcbf8115f4ce2b2fc39f18b3cbc7877c
camilo aliaga <camilo.aliaga.silva@gmail.com>
2023-02-08 22:29:32 -0300

    :spakles: completar la logica del componente login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,27 +2,56 @@ import { useState } from "react"
 
 
 
-
-
 const Login = () => {
   const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const handleSubmit = (e)=>{
+e.preventDefault()
+if(email.trim() === 'adl@desafiolatam.com' && password.trim()  === '252525'){
+  console.log('Los datos son correctos')
+}else{
+  console.log('Los datos son incorrectos')
+}
+  }
   return (
     <div>
 <h1>Desafio - Eventos y componentes</h1>
-<form>
+<form onSubmit={handleSubmit}>
 
 <div className="mb-3">
-  <label htmlFor="email" className="form-label">Email address</label>
-  <input type="email" className="form-control" id="email" placeholder="Ingresa tu correo"
+  <label htmlFor="email" className="form-label">
+    Email address
+  </label>
+  <input 
+  type="email" 
+  className="form-control" 
+  id="email" 
+  placeholder="Ingresa tu correo"
   value={email}
   onChange={e=>setEmail(e.target.value)}
   />
 </div>
 
 
+<div className="mb-3">
+  <label htmlFor="password" className="form-label">
+    Password
+    </label>
+  <input 
+  type="password" 
+  className="form-control" 
+  id="password" 
+  placeholder="*********"
+  value={password}
+  onChange={e=>setPassword(e.target.value)}
+  />
+</div>
+
+<button className="btn btn-dark" disabled={!email.trim() ||!password.trim()}>Iniciar sesion
+</button>
 </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login
